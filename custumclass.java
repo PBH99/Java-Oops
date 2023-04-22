1f64bdf5dd947c82dce93974ee01f6514a29be38
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-04-23 01:13:27 +0530

    Pr1 - Custom Class
    First custom class ( How to create a class )

---
+++ custumclass.java
@@ -0,0 +1,17 @@
+package oops;
+
+class jai{
+	int roll=20;
+	
+}
+public class custumclass {
+
+	public static void main(String[] args) {
+		 
+		jai ob=new jai();
+		System.out.print(ob.roll);
+		
+		
+	}
+
+}
